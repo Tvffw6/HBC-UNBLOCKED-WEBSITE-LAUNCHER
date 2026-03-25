b3fc575629fd24f8d19b276648069d5abd21425c
Tvffw6 <bendjiriouy@gmail.com>
2026-03-25 17:01:53 +0100

    Create sites.js with site information
    Added an array of site objects with details.

---
+++ sites.js
@@ -0,0 +1,20 @@
+const sites = [
+  {
+    name: "Google",
+    description: "Moteur de recherche",
+    url: "https://google.com",
+    icon: "assets/icons/google.png"
+  },
+  {
+    name: "YouTube",
+    description: "Vidéos en ligne",
+    url: "https://youtube.com",
+    icon: "assets/icons/youtube.png"
+  },
+  {
+    name: "GitHub",
+    description: "Code et projets",
+    url: "https://github.com",
+    icon: "assets/icons/github.png"
+  }
+];
